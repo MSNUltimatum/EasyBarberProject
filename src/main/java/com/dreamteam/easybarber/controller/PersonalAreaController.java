fe3abbf5c1f61bd08378fc144b7a4ddd901a36cc
MsnUlti <egrsar2000@gmail.com>
2020-07-24 12:45:42 +0400

    Instagramm bug

--- src/main/java/com/dreamteam/easybarber/controller/PersonalAreaController.java
+++ src/main/java/com/dreamteam/easybarber/controller/PersonalAreaController.java
@@ -83,6 +83,8 @@ public class PersonalAreaController {
         }
         if(username != null && !username.equals(""))
             user.setUsername(username);
+        if(instagram != null && !instagram.equals(""))
+            user.setInstagram(instagram);
         userRepo.save(user);
         return "redirect:/personalArea";
     }
