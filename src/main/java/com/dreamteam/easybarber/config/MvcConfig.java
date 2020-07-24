fc2b2a3988eaa23ea60a6d604d29b6926c4cd020
MsnUlti <egrsar2000@gmail.com>
2020-07-24 17:10:44 +0400

    Photo bug

--- src/main/java/com/dreamteam/easybarber/config/MvcConfig.java
+++ src/main/java/com/dreamteam/easybarber/config/MvcConfig.java
@@ -1,15 +1,10 @@
 package com.dreamteam.easybarber.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -23,13 +18,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
-                .addResourceLocations("file:/" + uploadPath + File.separator);
-    }
-
-    @Bean
-    public Path sourcesPath() {
-        return Paths.get(System.getProperty("catalina.home")+ File.separator + "webapps"+
-                File.separator + "expanded" /*"ROOT"*/+
-                File.separator + "myresources");
+                .addResourceLocations("file:/" + uploadPath + "/");
     }
 }
